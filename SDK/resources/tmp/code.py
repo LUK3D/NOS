0f26916136b257c44dfe805c8184bbf7b21c19f4
Filipe Lukebana <31851972+LUK3D@users.noreply.github.com>
2021-07-16 18:33:53 +0100

    Updated nos-cli

--- SDK/resources/tmp/code.py
+++ SDK/resources/tmp/code.py
@@ -1,13 +1,10 @@
 
-n1=0
-n2=0
-n3=0
-i=""
-nome=input("Informe um nome")
-i=input("Informe o valor de N1 ")
-n1=int(i)
-i=input("Informe o valor de N2 ")
-n2=int(i)
-i=input("Informe o valor de N3 ")
-n3=int(i)
-print("A édia é", (n1+n2+n3)/3)
+entrada=""
+i=0
+entrada=input("Informe um valor qualquer: ")
+i = 0 
+while(i< 12):
+	print(i,"=>", entrada)
+	i+=1 
+
+
