a8aeb642c9c3f05d58b946a45234160ecf2516c1
Filipe Lukebana <filipelukebana@gmail.com>
2021-10-12 14:57:04 +0100

    UPDATE: Melhorias na tradução dos comandos

--- compiler.luk.v1.0.py
+++ compiler.luk.v1.0.py
@@ -1,3 +1,4 @@
+from functools import cache
 import json
 import re
 from pprint import pprint
@@ -23,6 +24,9 @@ def join_tuple_string(strings_tuple: Tuple) -> str:
     """function that converts tuple to string"""
     return ' '.join(strings_tuple)
 
+def remove_empty_from_list(_list: list) -> list:
+    """function that removes whitespaces values from lists"""
+    return [x for x in _list if x.strip()]
 
 # TODO refazer toda a lógica de tradução,
 #  porque agora ja temos tudo informado. so precisamos traduzir
@@ -30,46 +34,88 @@ def nos_to_python(_commands: List):
     """método para conversão do código nós para python"""
     script = []
     indentationSteps = 0
-    for commad in _commands:
-        DBG().debug(f"{commad}")
-        code_line = ""
-        if commad["description"] is not None:
-            if not commad["description"]["ignore_on_translate"]:
-                code_line = (
-                        indent_code(indentationSteps) +
-                        commad["description"]["command"] +
-                        commad["instructions"]
-                )
-            if commad["description"]["end"] is not None:
-                code_line += commad["description"]["end"]
-        else:
-            code_line = (
-                    indent_code(indentationSteps) +
-                    commad["command"] +
-                    commad["instructions"]
-            )
-        command = str(commad["command"]).strip()
+    # NOVA LOGICA DE TRADUCAO
+    for n_cmd in _commands:
+        if(len(n_cmd)>0):
+            print(f"-------------------------------------------------------------------------------------------------------------------------")
+            code_line = ""
+            """Bloco de codigo para traduzir os comandos reconhecidos pelo sistem, ou seja, que se encontram no dicionário. """
+            if  n_cmd["description"]:
+                if(n_cmd["description"]["ignore_on_translate"]):
+                    continue
+            cmd_tmp = n_cmd["original"]
+            commands = n_cmd["commands"]
+            command = n_cmd["command"]
+            replace_pos = n_cmd["translation_origins"]
+            final_comands_list = "(".join(replace_pos.split("( (")).split("(")
 
-        # verificando se estamos a entrar ou sair no escopo de uma função.
-        if len(command) > 0:
-            if commad["description"] is None or command[-1] == "{":
-                if command == "{":
-                    code_line = ""
-                    indentationSteps += 1
-            if command == "}" or command[-1] == "}":
-                indentationSteps -= 1
-                code_line = ""
+            final_to_be_replaced = remove_empty_from_list(replace_pos.split("("))
 
-        # removendo o ';' do código
-        if len(code_line) > 0:
-            if code_line[-1] == ";":
-                code_line = code_line[:-1]
-        else:
-            continue
+            final_to_replce = []
 
-        # adicionando o comando final a lista
-        script.append(code_line)
+            for idx, val in enumerate(final_comands_list):
+                final_to_replce.append(val.strip().split(" ")[0])
+            
+            try:
+                idx= 0
+                for idx, val in enumerate(final_to_replce):
+                    if(val):
+                        cmd_tmp = commands[idx]["command"].join(final_to_be_replaced[idx].strip().split(val)).join(cmd_tmp.split(val))
 
+                        if(re.match("^([aA-z_Z])+( )+([aA-z_Z])+( )+([aA-z_Z])+", cmd_tmp)):
+                            cmd_tmp = (commands[idx]["command"] + ([x for x in re.split(r'^([aA-z_Z])+( )+([aA-z_Z])+', cmd_tmp.strip()) if len(x)>1])[0])
+            
+            except Exception as _erro:
+                print("ERRO! ",_erro)
+
+
+
+            
+            code_line = (indent_code(indentationSteps) +cmd_tmp)
+        
+            
+            
+            
+        
+            command = str(code_line).strip()
+
+            # verificando se estamos a entrar ou sair no escopo de uma função.
+            if len(command) > 0:
+                if n_cmd["description"] is None or command[-1] == "{" or  command[-1] == "{:":
+                    if command == "{" or  command[-1] == "{:":
+                        code_line = code_line.rstrip()[:-1]
+                        indentationSteps += 1
+                if command == "}" or command.rstrip()[-1] == "}":
+                    if command == "}":
+                        code_line = code_line.rstrip()[:-1]
+                        indentationSteps -= 1
+                    
+
+            # removendo o ';' do código
+            if len(code_line) > 0:
+                if code_line[-1] == ";":
+                    code_line = code_line[:-1]
+            else:
+                continue
+            
+            #Verificando se o comando deve terminar com algum caractere definido no dicionario
+            if  n_cmd["description"]:
+                if(n_cmd["description"]["type"] == "bloc"): #Caso essa codicao seja verdadeira deve se remover os parenteses iniciais na funcao
+                    code_line = re.split(r'[\(]', code_line).join("")
+                if (n_cmd["description"]["end"] is not None):
+                    code_line += n_cmd["description"]["end"]
+            code_line =":".join(code_line.split("{:"))
+            script.append(code_line)
+            
+
+            # adicionando o comando final a lista
+            print(f"{code_line}")
+
+            print(f"-------------------------------------------------------------------------------------------------------------------------")
+
+
+            
+            print("FINAL COD->", "\n".join(script))
     DBG().debug(f"{script}")
     return "\n".join(script)
 
@@ -89,14 +135,14 @@ def verify_command(_noscode: str):
     regex = re.compile(r"([aA-z_Z ]*(\(|\(\)|\( *))")
     # O regex acima retorna tuples, então precisa ser convertido para lista
     formated_command_1 = map(join_tuple_string, regex.findall(_noscode))
-    formated_command_1 = " ".join(formated_command_1)
+    formated_command_1 = " ".join(formated_command_1)[:-1]
 
-    # Pegando apenas palavras e ignorando qualquer carácter especial
+    # Pegando apenas palavras e ignorando qualquer caracter especial
     regex2 = re.compile("[A-Za-z_]*")
     formated_command_2 = regex2.findall(formated_command_1)
 
     # Limpando a lista e deixando apenas os valores nao nulos ou vazios.
-    final_formated_commands = [x for x in formated_command_2 if x]
+    final_formated_commands = remove_empty_from_list (formated_command_2) 
 
     final = {
         "command": _noscmd,
@@ -188,9 +234,11 @@ if __name__ == '__main__':
  ├─────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
  │ Data de lançamento: 13-Julho-2021 21:40                                                                     |
  ├─────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
- │ Author: Filipe Lukebana                                                                                     |
+ │ Author e mantainer: Filipe Lukebana                                                                         |
  ├─────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
  │ website:https://www.nos.luk3d.com                                                                           │
+ ├─────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+ │ Colaborador e mantainer: Nurul-GC   website:https://github.com/Nurul-GC                                     |
  ├─────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
  |                                  [ COMANDOS USADOS NO COMPILADOR ]                                          |
  |   EXECUTAR UM SCRIPT NOS: [ run nome_do_script.nos ]                                                        |
