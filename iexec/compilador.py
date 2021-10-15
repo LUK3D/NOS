1da6d24416e56799ece4d5575a50e28a20cc3813
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-15 08:29:59 +0200

    Update iexec/compilador.py

--- iexec/compilador.py
+++ iexec/compilador.py
@@ -1,17 +1,21 @@
-from functools import cache
 import json
-from logging import exception
+import os
 import re
 from pprint import pprint
+from sys import exit, argv
 from typing import Tuple, List
-from sys import argv, exit
 
 from debugger import DBG
+
+# LENDO O DICIONÁRIO COM AS PALAVRAS RESERVADAS DE {NOS}
+with open('../core/dictionary.json') as f:
+    RESERVED = json.load(f)
 
 
 def save_final_code(code: str):
     """método para salvar o código python gerado em um arquivo"""
-    with open("output.py", "w") as py_file:
+    os.makedirs(".temp", exist_ok=True)
+    with open(".temp/output.py", "w") as py_file:
         py_file.write(code)
         return "Arquivo gerado em: output.py"
 
@@ -22,11 +26,12 @@ def indent_code(_times: int):
 
 
 def join_tuple_string(strings_tuple: Tuple) -> str:
-    """function that converts tuple to string"""
+    """função que converte tupla em string"""
     return ' '.join(strings_tuple)
 
+
 def remove_empty_from_list(_list: list) -> list:
-    """function that removes whitespaces values from lists"""
+    """função que remove valores vazios das listas"""
     return [x for x in _list if x.strip()]
 
 
@@ -34,14 +39,13 @@ def nos_to_python(_commands: List):
     """método para conversão do código nós para python"""
     script = []
     indentationSteps = 0
-    # NOVA LOGICA DE TRADUCAO
     for n_cmd in _commands:
-        if(len(n_cmd)>0):
-            print("-"*100)
+        if len(n_cmd) > 0:
+            print("-" * 100)
             code_line = ""
-            # Bloco de codigo para traduzir os comandos reconhecidos pelo sistem, ou seja, que se encontram no dicionário.
-            if  n_cmd["description"]:
-                if(n_cmd["description"]["ignore_on_translate"]):
+            # Bloco de codigo para traduzir os comandos  que se encontram no dicionário.
+            if n_cmd["description"]:
+                if n_cmd["description"]["ignore_on_translate"]:
                     continue
 
             cmd_tmp = n_cmd["original"]
@@ -55,21 +59,21 @@ def nos_to_python(_commands: List):
 
             for idx, val in enumerate(final_comands_list):
                 final_to_replce.append(val.strip().split(" ")[0])
-            
+
             try:
-                idx= 0
+                idx = 0
                 for idx, val in enumerate(final_to_replce):
-                    if(val):
+                    if val:
                         cmd_tmp = commands[idx]["command"].join(final_to_be_replaced[idx].strip().split(val)).join(cmd_tmp.split(val))
 
-                        if(re.match("^([aA-z_Z])+( )+([aA-z_Z])+( )+([aA-z_Z])+", cmd_tmp)):
-                            cmd_tmp = (commands[idx]["command"] + ([x for x in re.split(r'^([aA-z_Z])+( )+([aA-z_Z])+', cmd_tmp.strip()) if len(x)>1])[0])
+                        if re.match(r"^([aA-z_Z])+( )+([aA-z_Z])+( )+([aA-z_Z])+", cmd_tmp):
+                            cmd_tmp = (commands[idx]["command"] + ([x for x in re.split(r'^([aA-z_Z])+( )+([aA-z_Z])+', cmd_tmp.strip()) if len(x) > 1])[0])
             except Exception as _erro:
-                print("ERRO! ",_erro)
+                print("ERRO! ", _erro)
 
             # traduzindo os operadores logicos
-            code_line = (indent_code(indentationSteps) +cmd_tmp)
-            rex = re.compile("(\|\|)|(\&\&)|((\!)[a-zA-Z\(\)].*)")
+            code_line = (indent_code(indentationSteps) + cmd_tmp)
+            rex = re.compile(r"(\|\|)|(&&)|((!)[a-zA-Z()].*)")
 
             for item in rex.findall(code_line):
                 try:
@@ -80,28 +84,28 @@ def nos_to_python(_commands: List):
                 except Exception as _erro:
                     print(_erro)
 
-            # Traduzindo termos internos reservados do comando ou da funcao
             try:
+                # Traduzindo termos internos reservados do comando ou da funcao
                 for cmd in n_cmd["commands"]:
                     for cmd_op in cmd["internals"]:
                         code_line = cmd_op["command"].join(code_line.split(cmd_op["key"]))
                     try:
                         # Removendo os parenteses dos comandos com esta regra gramatical
-                        if cmd["no_parentheses"] == True:
-                            code_line = code_line.replace("("," ",1)
+                        if cmd["no_parentheses"]:
+                            code_line = code_line.replace("(", " ", 1)
                             last_char_index = code_line.rfind(")")
-                            code_line = code_line[:last_char_index] + " " + code_line[last_char_index+1:]
+                            code_line = code_line[:last_char_index] + " " + code_line[last_char_index + 1:]
                         elif cmd["type"] == "bloc":
-                            print("BUGOU-----------------------------------", cmd)
+                            print("[ FALHA ]-----------------------------------\n", cmd)
                     except Exception as _erro:
-                        print (_erro)
+                        print(_erro)
             except Exception as _erro:
                 print(_erro)
 
             # verificando se estamos a entrar ou sair no escopo de uma função.
-            if re.match("(.*{)$",code_line):
+            if re.match("(.*{)$", code_line):
                 indentationSteps += 1
-            elif re.match("(.*})$",code_line):
+            elif re.match("(.*})$", code_line):
                 indentationSteps -= 1
                 # if command == "}" or command.rstrip()[-1] == "}":
                 #     if command == "}":
@@ -113,29 +117,19 @@ def nos_to_python(_commands: List):
                     code_line = code_line[:-1]
             else:
                 continue
-            
-            #Removendo as chaves e qualquer tipo de finalizacao da linha sem suporte.. EX ({, }, {:, (, :),})
-            code_line = re.sub(r"(\(\:)|(\{\:)|({)|(\:\:)$",":",code_line)
-            code_line = re.sub(r"(\})$","",code_line)
+
+            # Removendo as chaves e qualquer tipo de finalizacao da linha sem suporte.. EX ({, }, {:, (, :),})
+            code_line = re.sub(r"(:)|({:)|({)|(:$)", ":", code_line)
+            code_line = re.sub(r"(})$", "", code_line)
             # adicionando o comando final a lista
             try:
                 code_line += n_cmd["description"]["end"]
             except Exception as _erro:
                 print("ERRO: ", _erro)
-            # code_line = ":".join()
-                
-            
-            # code_line =":".join(code_line.split("{:"))
-            # if(len(code_line)>1):
-            #     code_line =":".join(code_line.split("):")) if code_line[-2] ==")" else code_line
-
-            # if n_cmd["description"]:
-            #     if (n_cmd["description"]["type"] == "func"):
-            #         code_line += "):"
 
             script.append(code_line)
             print(f"{code_line}")
-            print('-'*100)
+            print('-' * 100)
 
     DBG().debug(f"{script}")
     return "\n".join(script)
@@ -143,18 +137,15 @@ def nos_to_python(_commands: List):
 
 def verify_command(_noscode: str):
     """processa o comando e retorna o objecto que o representa no dicionário do {NOS}"""
-
     tmp_cmd_listados = []
     for operator in RESERVED["attrib_operadores"]["command"]:
-        # for cmd_in in re.split(f'({",".join(RESERVED["command_scaped"])})', _noscode):
         for cmd_in in _noscode.split(operator):
             cmd_in_tmp = cmd_in.strip().split(" ")
-            if len(cmd_in_tmp)>1:
-                print("OPCOES:", cmd_in_tmp, "".join(re.split("\;$", cmd_in_tmp[len(cmd_in_tmp)-1])))
-                tmp_cmd_listados.append("".join(re.split("\;$", cmd_in_tmp[len(cmd_in_tmp)-1])))
-            elif len(cmd_in_tmp)>0:
-                tmp_cmd_listados.append("".join(re.split("\;$", cmd_in_tmp[0])))
-
+            if len(cmd_in_tmp) > 1:
+                print("OPCOES:", cmd_in_tmp, "".join(re.split(r";$", cmd_in_tmp[len(cmd_in_tmp) - 1])))
+                tmp_cmd_listados.append("".join(re.split(r";$", cmd_in_tmp[len(cmd_in_tmp) - 1])))
+            elif len(cmd_in_tmp) > 0:
+                tmp_cmd_listados.append("".join(re.split(r";$", cmd_in_tmp[0])))
     try:
         tmp_cmd_listados = list(set(tmp_cmd_listados))
         constante = RESERVED[tmp_cmd_listados[0].strip()]
@@ -168,10 +159,7 @@ def verify_command(_noscode: str):
     _noscmd = cmd_listados[0] if (len(cmd_listados) > 1) else _noscode
     no_code_split = _noscode.split(_noscmd)
 
-    # regex = "(.*[\("+"".join(RESERVED["attrib_operadores"]["command_scaped"])+"])"
-    # Pega todos os comandos que terminam em (),(,
-    regex = re.compile(r"([aA-z_Z ]*(\(|\(\)|\(|\{ *))")
-    # O regex acima retorna tuples, então precisa ser convertido para lista
+    regex = re.compile(r"([aA-z_Z ]*((|\()|\(|{ *))")
     formated_command_1 = map(join_tuple_string, regex.findall(_noscode))
     formated_command_1 = " ".join(formated_command_1)[:-1]
 
@@ -180,7 +168,7 @@ def verify_command(_noscode: str):
     formated_command_2 = regex2.findall(formated_command_1)
 
     # Limpando a lista e deixando apenas os valores nao nulos ou vazios.
-    final_formated_commands = remove_empty_from_list (formated_command_2) 
+    final_formated_commands = remove_empty_from_list(formated_command_2)
 
     final = {
         "command": _noscmd,
@@ -198,9 +186,7 @@ def verify_command(_noscode: str):
                 final["commands"].append(search)
         except Exception as _erro:
             print(_erro)
-
     try:
-        
         if len(cmd_listados) > 1:
             search = RESERVED[_noscmd]
             if search:
@@ -208,16 +194,15 @@ def verify_command(_noscode: str):
             else:
                 final["description"] = None
     except Exception as _erro:
-                print(_erro)
-            
+        print(_erro)
 
     DBG().debug(f"FINAL ----------> {final}")
     return final
 
 
-def run_file(file):
+def run_file(_file: str):
     """funcao para executar um arquivo nos"""
-    code_nos = open(file).readlines()
+    code_nos = open(_file).readlines()
     code_py = []
 
     commenting = False
@@ -226,30 +211,29 @@ def run_file(file):
 
         # Removendo os comentarios de uma unica linha
         try:
-            print("REMOVENDO ESSA LINHA:", linha, re.match("([^\\:]|^)\/\/.*$", linha) )
-            linha = re.sub("([^\\:]|^)\/\/.*$","",linha)
+            print("REMOVENDO ESSA LINHA:", linha, re.match(r"([^:]|^)//.*$", linha))
+            linha = re.sub(r"([^\\:]|^)//.*$", "", linha)
         except Exception as _erro:
             print(_erro)
-            
+
         # Removendo os comentarios com multiplas linhas
-        if re.match("\/\*[\s\S]*?",linha):
+        if re.match(r"/\*[\s\S]*?", linha):
             commenting = True
-        elif re.match("\*\/$",linha):
+        elif re.match(r"\*/$", linha):
             commenting = False
             try:
-                linha = re.sub("\*\/$","",linha)
+                linha = re.sub(r"\*/$", "", linha)
             except Exception as _erro:
                 print(_erro)
         elif linha != "" and not commenting:
             vc = verify_command(linha)
             if len(vc) > 1:
                 code_py.append(vc)
-        
     return code_py
 
 
 def run():
-    """FUNCAO PRINCIPAL"""
+    """funcao principal"""
     while True:
         cmd_process = input("{NOS} -> ").split(" ")
         if cmd_process[0] == "run":
@@ -270,7 +254,6 @@ def run():
 
 
 if __name__ == '__main__':
-    # perdão tirei o try daqui para puder capturar melhor as falhas!
     print("""
 ┌=============================================================================================================┐
 |                                      ##                               ##                                    |
@@ -303,8 +286,6 @@ if __name__ == '__main__':
 |   TERMINAR O COMPILADOR:  [ q ] OU [ exit ]                                                                 |
 |_____________________________________________________________________________________________________________|
 """)
-
-    # LENDO O DICIONÁRIO COM AS PALAVRAS RESERVADAS DE {NOS}
-    with open('./core/dictionary.json') as f:
-        RESERVED = json.load(f)
-        run()
+    run()
+elif len(argv) > 1:
+    run_file(argv[1])
